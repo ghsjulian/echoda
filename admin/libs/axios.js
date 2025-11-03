788cfc38855e0328cb5cbb9ccb2739d30ae80b05
ghsjulian <ghsjulian@gmail.com>
2025-11-03 14:08:42 +0600

    Added Frontend

--- admin/libs/axios.js
+++ admin/libs/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const api = "https://auth-app-ie66.onrender.com/api/v1/user"
-const api = "http://localhost:3000/api/v1/admin";
+//const api = "http://localhost:3000/api/v1/admin";
+const api = "https://echoda.vercel.app/api/v1/admin";
 
 const axiosConfig = axios.create({
     baseURL: api,
--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const HOST = process.env.HOST || "127.0.0.1";
 app.use(express.json({ limit: "1000mb" }));
 app.use(
     cors({
-        origin: ["http://localhost:5000", "http://localhost:5001","https://echoda-admin.netlify.app"],
+        origin: ["http://localhost:5000", "http://localhost:5001","https://echoda.vercel.app"],
         methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
